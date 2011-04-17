ec0b3784f0c560416bab8c676323e7fa4d2397a7
NoxArt <petruzelka@nox-art.cz>
2011-04-17 22:36:28 +0200

    created Parser interface

---
+++ source/parser/Parser.h
@@ -0,0 +1,55 @@
+/* 
+ * File:   Parser.h
+ * Author: Nox
+ *
+ * Created on 18. březen 2011, 16:53
+ */
+
+#ifndef PARSER_H
+	#define	PARSER_H
+
+#include <map>
+#include <vector>
+#include <string>
+#include "enums.h"
+#include "Item.h"
+#include "Evaluator.h"
+
+using namespace std;
+
+namespace CubeSoft {
+
+    namespace Calculator {
+
+		typedef vector<Item*>::iterator itemIterator;
+
+        class Parser {
+            public:
+                Parser();
+
+				Parser* addInput(double value);
+				Parser* addInput(eOperator op);
+                Parser* addInput(Item* item);
+				Parser* clearInput();
+				
+				void buildTree();
+				Item* getRoot();
+            private:
+                void close();
+
+				Item* root;
+                vector<Item*> input;
+
+                static map<eOperator, int> createPrecedencyTable();
+                static const map<eOperator, int> precedencyTable;
+
+				Item* getNextOfType(itemIterator iter, itemIterator end, eType type);
+				itemIterator addItem(Item *starter, itemIterator iter);
+        };        
+        
+    }
+
+}
+
+#endif	/* PARSER_H */
+
